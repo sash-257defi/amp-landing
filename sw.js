fa83523d33589d31b27118d737c288116c3b71de
sash-binary <sash@binary.com>
2020-04-23 12:28:47 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-178e448833e15cd6da96.js"
+    "url": "webpack-runtime-a49f86571418c0b7535f.js"
   },
   {
     "url": "framework-4adb46e364577e67729f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d4bf000cb380e549cce75aa76fe9f49c"
+    "revision": "d1906a89d37319feac274aa9cacf3574"
   },
   {
     "url": "google-fonts/s/roboto/v20/KFOlCnqEu92Fr1MmSU5fBBc4.woff2",
@@ -60,7 +60,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "bdca3001e4132b11086fd41eda3b23c2"
+    "revision": "b3e4098950d10979b9f95c9d89a07e6d"
   },
   {
     "url": "manifest.webmanifest",
